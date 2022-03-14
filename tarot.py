37c841f0455e018e009fb2d019a402b4eb397603
tfitzner <5036869+tfitzner@users.noreply.github.com>
2022-03-14 17:18:33 -0400

    added first function for pulling three cards at random.

--- tarot.py
+++ tarot.py
@@ -19,6 +19,13 @@ full_arcana = major_arcana + minor_arcana
 
 print("Welcome to Tarot Reader Express")
 #user_reading = input("What type of reading are you looking for?")
+def pull_three_cards(reading):
+    first_card = full_arcana[randint(0, 77)]
+    second_card = full_arcana[randint(0, 77)]
+    third_card = full_arcana[randint(0, 77)]
+    print(first_card)
+    print(second_card)
+    print(third_card)
 
-print(str(minor_arcana[randint(0, 13)]) + " of " + str(minor_suits[randint(0, 3)]))
+pull_three_cards("Love")
 #print(full_arcana)
